cb148e100481956f95ba2512df0832a61089eb84
Yaroslav <yaroslavgiv31@gmail.com>
2026-08-18 10:09:00 +0300

    test(auth): mock secure storage during app startup

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,9 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hotel_booking_app/features/auth/presentation/pages/auth_page.dart';
 import 'package:hotel_booking_app/main.dart';
 
 void main() {
+  const secureStorageChannel = MethodChannel(
+    'plugins.it_nomads.com/flutter_secure_storage',
+  );
+
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUp(() {
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMethodCallHandler(secureStorageChannel, (_) async => null);
+  });
+
+  tearDown(() {
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMethodCallHandler(secureStorageChannel, null);
+  });
+
   testWidgets('renders the hotel booking authentication flow', (
     WidgetTester tester,
   ) async {
